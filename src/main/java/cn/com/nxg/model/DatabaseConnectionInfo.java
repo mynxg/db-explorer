083dc557279e50c13bdc9c3f6bb03c586dd58c61
mynxg <mynxg@qq.com>
2025-02-26 22:48:11 +0800

    feat：修改为jdk17、springboot3.0

--- src/main/java/cn/com/nxg/model/DatabaseConnectionInfo.java
+++ src/main/java/cn/com/nxg/model/DatabaseConnectionInfo.java
@@ -1,11 +1,11 @@
 package cn.com.nxg.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Data
 public class DatabaseConnectionInfo {
